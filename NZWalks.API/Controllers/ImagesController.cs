4ed23f4b21273e4770b1fc8d85b9ae64b8c6ba42
Chineze Okadigbo <72756219+neze14@users.noreply.github.com>
2025-06-26 16:04:57 +0100

    Section 9: Image Upload

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:44372/api/v1/images
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        //POST: api/v1/images/upload
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadImage([FromForm] ImageUploadRequestDto requestDto)
+        {
+            ValidateFileUpload(requestDto);
+
+            if (ModelState.IsValid)
+            {
+                // convert dto to domain model
+                var imageDomain = new Image
+                {
+                    File = requestDto.File,
+                    FileExtension = Path.GetExtension(requestDto.File.FileName),
+                    FileDescription = requestDto.FileDescription,
+                    FileName = requestDto.FileName,
+                    FileSizeInBytes = requestDto.File.Length,
+                };
+
+                await imageRepository.UploadAsync(imageDomain);
+
+                return Ok(new
+                {
+                    message = "Image uploaded successfully", 
+                    imageDomain
+                });
+
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtrensions = new string[] { ".jpg", ".jpeg", ".png", ".pdf" };
+
+            if (!allowedExtrensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File is more than 10mb. Please upload a smaller size.");
+            }
+        }
+    }
+}
---
+++ NZWalks.API/Repositories/LocalImageRepository.cs
@@ -0,0 +1,44 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class LocalImageRepository : IImageRepository
+    {
+        private readonly IWebHostEnvironment webHostEnvironment;
+        private readonly IHttpContextAccessor contextAccessor;
+        private readonly NZWalksDbContext dbContext;
+
+        public LocalImageRepository(IWebHostEnvironment webHostEnvironment,
+            IHttpContextAccessor contextAccessor,
+            NZWalksDbContext dbContext)
+        {
+            this.webHostEnvironment = webHostEnvironment;
+            this.contextAccessor = contextAccessor;
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Image> UploadAsync(Image image)
+        {
+            var localFilePath = Path.Combine(webHostEnvironment.ContentRootPath, "Images", 
+                image.FileName + image.FileExtension);
+
+            // upload img to local path
+            using var stream = new FileStream(localFilePath, FileMode.Create);
+
+            await image.File.CopyToAsync(stream);
+
+            // https://localhost:44372/images/image.jpg
+            var urlFilePath = $"{contextAccessor.HttpContext.Request.Scheme}://{contextAccessor.HttpContext.Request.Host}{contextAccessor.HttpContext.Request.PathBase}/Images/{image.FileName}{image.FileExtension}";
+
+            image.FilePath = urlFilePath;
+
+            // add img to the images table
+            await dbContext.Images.AddAsync(image);
+            await dbContext.SaveChangesAsync();
+
+            return image;
+        }
+
+    }
+}
