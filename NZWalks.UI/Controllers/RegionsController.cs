893f65f3402d95678d5d5a2eece92f98f4991697
Chineze Okadigbo <72756219+neze14@users.noreply.github.com>
2025-07-07 20:38:07 +0100

    Section 12: Implement GET

--- NZWalks.UI/Controllers/RegionsController.cs
+++ NZWalks.UI/Controllers/RegionsController.cs
@@ -1,13 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.UI.Models.DTO;
+using System.Threading.Tasks;
 
 namespace NZWalks.UI.Controllers
 {
     public class RegionsController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public RegionsController(IHttpClientFactory httpClientFactory)
         {
-            // Get all regions from web api
-            return View();
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<RegionDto> response = new List<RegionDto>();
+
+            try
+            {
+                // Get all regions from web api
+                var client = httpClientFactory.CreateClient();
+
+                var httpResponseMessage = await client.GetAsync("https://localhost:44372/api/v1/Regions/get-all-regions");
+
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                response.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<RegionDto>>());
+            }
+            catch (Exception ex)
+            {
+                // log exception
+                throw;
+            }
+
+            return View(response);
         }
     }
 }
